8f2522730096906451804feeecf9cd6f0a18de07
Ashley Doerfler <ashleyndoerfler@gmail.com>
2024-02-19 21:40:39 -0800

    Format location, state,zip, and weather forecast onto screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,27 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Daily Weather Forecast'),
     );
   }
 }
@@ -71,12 +57,35 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Text(location["city"]!);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(75.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Location',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text('City: ${location["city"]}'),
+              Text('State: ${location["state"]}'),
+              Text('Zip: ${location["zip"]}'),
+              SizedBox(height: 20),
+              const Text(
+                'Current Weather Forecast',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text('Temperature: ${forecast["temperature"]}°C'),
+              Text('Detailed Forecast: ${forecast["detailedForecast"]}'),
+              Text('Wind Speed/Direction: ${forecast["windSpeed"]} m/s ${forecast["windDirection"]}'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
